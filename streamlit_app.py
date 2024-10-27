25cf2ae6812858a1a0c678ac6b9ac6d789c9556d
HD-MOE <tok_hong_da@moe.edu.sg>
2024-10-27 14:09:50 +0800

    import sqlite

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from crewai import Crew
 from ecg_agents import ECGAgents, StreamToExpander

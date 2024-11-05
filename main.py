7ce41185ffd9686587fb25ba4631463444b000ef
HD-MOE <tok_hong_da@moe.edu.sg>
2024-11-05 22:21:55 +0800

    changes to tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from tools.utility import check_password
+from utility import check_password
 
 # Do not continue if check_password is not True.  
 if not check_password():  
